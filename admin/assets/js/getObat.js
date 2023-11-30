f170d0090906c72b386fe10a312235312eb066a5
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2023-11-30 22:04:30 +0700

    Feature obat: update content

--- admin/assets/js/getObat.js
+++ admin/assets/js/getObat.js
@@ -38,11 +38,29 @@ export const tableObat = `
     >
   </div>
 </td>
-<td class="h-px w-px whitespace-nowrap">
+<td class="px-6 py-3 whitespace-nowrap">
+    <button class="btn btn-outline-primary btn-sm" onclick="editObat('#IDOBAT#')">Edit</button>
+    <button class="btn btn-outline-danger btn-sm" onclick="deleteObat('#IDOBAT#')">Delete</button>
+  </td>
 </td>
 </tr>
 `;
 
+// Function to handle edit action
+window.editObat = (idObat) => {
+  // Logic to navigate to the edit page or perform edit action
+  console.log("Edit button clicked for Obat ID:", idObat);
+};
+
+// Function to handle delete action
+window.deleteObat = (idObat) => {
+  // Logic to confirm deletion and perform delete action
+  const confirmation = confirm("Are you sure you want to delete this Obat?");
+  if (confirmation) {
+    // Logic to delete the Obat, you can use fetch or any other method
+    console.log("Delete button clicked for Obat ID:", idObat);
+  }
+};
 
 export function responseData(results) {
     console.log(results);
@@ -59,3 +77,4 @@ export function isiRow(value) {
     .replace("#HARGA#", value.harga);
   addInner("tableDaftarObat", content);
 }
+
