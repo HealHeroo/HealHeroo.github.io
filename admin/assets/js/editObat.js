600bebb98689a3d77244e5529ffa0ec4b2887eb5
stefanymelda <juwitastefany13@gmail.com>
2023-12-04 14:58:32 +0700

    push update obat

--- admin/assets/js/editObat.js
+++ admin/assets/js/editObat.js
@@ -1,31 +1,18 @@
-export const isiData = (results) => {
-    const inputMapping = [
-      { id: "nama_obat", path: "obat.nama_obat" },
-      { id: "jenis_obat", path: "obat.jenis_obat" },
-      { id: "keterangan", path: "obat.keterangan" },
-      { id: "harga", path: "obat.harga" },
-    ];
-  
-    inputMapping.forEach(({ id, path, index, property }) => {
-      const inputElement = document.getElementById(id);
-      const value = getNestedValue(results, path, index, property);
-      inputElement.value = value;
-    });
-  };
-  
-  const getNestedValue = (obj, path, index, property) => {
-    const value = path
-      .split(".")
-      .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  
-    if (
-      Array.isArray(value) &&
-      value.length > index &&
-      value[index].hasOwnProperty(property)
-    ) {
-      return value[index][property];
-    }
-  
-    return value;
-  };
-  
+// Misalnya, data obat yang diterima seperti berikut:
+const dataObat = {
+  nama_obat: "Nama Obat",
+  jenis_obat: "Jenis Obat",
+  keterangan: "Keterangan Obat",
+  harga: "100000",
+};
+
+// Fungsi untuk mengisi formulir dengan data obat
+function isiFormulir(dataObat) {
+  document.getElementById("nama_obat").value = dataObat.nama_obat;
+  document.getElementById("jenis_obat").value = dataObat.jenis_obat;
+  document.getElementById("keterangan").value = dataObat.keterangan;
+  document.getElementById("harga").value = dataObat.harga;
+}
+
+// Panggil fungsi isiFormulir dengan data obat yang telah diambil
+isiFormulir(dataObat);
