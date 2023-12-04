fa6d8aa167699d41b49ccea7d790479339618cf4
stefanymelda <juwitastefany13@gmail.com>
2023-12-04 14:33:04 +0700

    update

--- admin/assets/js/fetchEditObat.js
+++ admin/assets/js/fetchEditObat.js
@@ -1,29 +1,4 @@
-// import { isiData } from "editObat.js";
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-// import { urlFetch } from "urlEditObat.js";
-
-// function get(target_url, responseFunction) {
-//   const myHeaders = new Headers();
-//   myHeaders.append("Authorization", getCookie("Authorization"));
-
-//   var requestOptions = {
-//     method: "GET",
-//     headers: myHeaders,
-//     redirect: "follow",
-//   };
-
-//   fetch(target_url, requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => responseFunction(JSON.parse(result)))
-//     .catch((error) => console.log("error", error));
-// }
-// window.editObat = (idObat) => {
-//     // Logic to fetch specific obat data for editing
-//     const editUrl = `https://asia-southeast2-peak-equator-402307.cloudfunctions.net/obat_healhero?id=${idObat}`;
-//     get(editUrl, isiData);
-//   };
-// get(urlFetch, isiData);
-
+// Import modul dengan path relatif
 import { isiData } from "./editObat.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { urlFetch } from "./urlEditObat.js";
@@ -43,4 +18,6 @@ function get(target_url, responseFunction) {
     .then((result) => responseFunction(JSON.parse(result)))
     .catch((error) => console.log("error", error));
 }
+
+// Panggil fungsi get untuk mendapatkan data obat dan isiData untuk menampilkan data
 get(urlFetch, isiData);
