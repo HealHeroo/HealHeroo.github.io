404714fe27a4a5a59dec19c999f3b84fa4c767ae
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-01-09 14:56:31 +0700

    Refactored postOrder function and added error handling

--- pengguna/assets/js/order.js
+++ pengguna/assets/js/order.js
@@ -1,4 +1,4 @@
-import { getValue } from "https://jscroot.github.io/element/croot.js";
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
